12c384831a6334f5b6a3977527d552da05e54c05
nv-vn <nv@cock.li>
2016-03-28 12:32:44 -0400

    Paved the way for different modes

--- src/main.ml
+++ src/main.ml
@@ -9,9 +9,17 @@ let () =
   let usage = "opam create [-noasis] [-nopam] [name]" in
   let oasis = ref true
   and opam  = ref true
-  and name  = ref None in
+  and name  = ref None
+  and mode  = ref `Gen in
   Arg.parse [
     "noasis", Arg.Clear oasis, "Don't output OASIS file";
-    "nopam",  Arg.Clear opam,  "Don't output OPAM repo files"
+    "nopam",  Arg.Clear opam,  "Don't output OPAM repo files";
+    "depend", Arg.Rest (fun s -> mode := `Depend s), "Add a dependency";
+    "refresh", Arg.Unit (fun _ -> mode := `Refresh), "Regenerate all files";
+    "main", Arg.String (fun s -> mode := `Main s), "Set the main module for compilation";
+    "modules", Arg.Rest (fun s -> mode := `Modules s), "Add modules to export";
+    "tests", Arg.Unit (fun _ -> mode := `Test), "Generate tests for the repository"
   ] (fun n -> name := Some n) usage;
-  UserInput.create !oasis !opam !name
+  match !mode with
+  | `Gen -> UserInput.create !oasis !opam !name
+  | _ -> print_endline "Not yet implemented!"
